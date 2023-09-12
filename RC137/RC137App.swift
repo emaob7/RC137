90b3cc17c9863a1debeca6bd096eee17afd34f87
emmaob7 <emanuelob7@gmail.com>
2023-09-12 15:50:47 -0400

    Initial Commit

---
+++ RC137/RC137App.swift
@@ -0,0 +1,17 @@
+//
+//  RC137App.swift
+//  RC137
+//
+//  Created by Emmanuel Ortigoza on 2023-09-12.
+//
+
+import SwiftUI
+
+@main
+struct RC137App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
